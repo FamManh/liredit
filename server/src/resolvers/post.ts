3b4d9529bad58da8b4b375381155560d96f9abd5
manh.pham <manh.pham@digicommercegroup.com>
2022-01-08 18:07:57 +0700

    post resolvers

--- server/src/resolvers/post.ts
+++ server/src/resolvers/post.ts
@@ -1,4 +1,4 @@
-import { Ctx, Query, Resolver } from "type-graphql";
+import { Arg, Ctx, Int, Mutation, Query, Resolver } from "type-graphql";
 import { MyContext } from "../types";
 import { Post } from "../entities/Post";
 
@@ -8,5 +8,51 @@ export class PostResolver {
   async posts(@Ctx() { em }: MyContext): Promise<Post[]> {
     const posts = await em.find(Post, {});
     return posts;
+  }
+
+  @Query(() => Post, { nullable: true })
+  async post(
+    @Arg("id", () => Int) id: number,
+    @Ctx() { em }: MyContext
+  ): Promise<Post | null> {
+    const post = await em.findOne(Post, { id });
+    return post;
+  }
+
+  @Mutation(() => Post)
+  async createPost(
+    @Arg("title", () => String) title: string,
+    @Ctx() { em }: MyContext
+  ): Promise<Post> {
+    const post = em.create(Post, { title });
+    await em.persistAndFlush(post);
+    return post;
+  }
+
+  @Mutation(() => Post, { nullable: true })
+  async updatePost(
+    @Arg("id") id: number,
+    @Arg("title", () => String, { nullable: true }) title: string,
+    @Ctx() { em }: MyContext
+  ): Promise<Post | null> {
+    const post = await em.findOne(Post, { id });
+    if (!post) return null;
+    if (typeof title !== undefined) {
+      post.title = title;
+      em.persistAndFlush(post);
+    }
+    return post;
+  }
+
+  @Mutation(() => Post, { nullable: true })
+  async deletePost(
+    @Arg("id") id: number,
+    @Ctx() { em }: MyContext
+  ): Promise<Post | null> {
+    const post = em.findOne(Post, { id });
+    if (!post) return null;
+    await em.nativeDelete(Post, { id });
+
+    return post;
   }
 }
